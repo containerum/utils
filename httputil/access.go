cfccc1165d8146be39fa54edde113f2c9725160b
Ilia Donchenko <iliad@me.com>
2018-06-21 12:15:45 +0300

    Check access only if not admin

--- httputil/access.go
+++ httputil/access.go
@@ -42,22 +42,24 @@ type AccessChecker struct {
 
 func (a *AccessChecker) CheckAccess(requiredAccess kubeModel.UserGroupAccess) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		project := ctx.Param(ProjectParam)
-		ns := ctx.Param(NamespaceParam)
+		if MustGetUserRole(ctx) != "admin" {
+			project := ctx.Param(ProjectParam)
+			ns := ctx.Param(NamespaceParam)
 
-		namespaceAccess, err := a.PermissionsClient.GetNamespaceAccess(ctx.Request.Context(), project, ns)
-		if err != nil {
-			gonic.Gonic(a.AccessError(), ctx)
-			return
+			namespaceAccess, err := a.PermissionsClient.GetNamespaceAccess(ctx.Request.Context(), project, ns)
+			if err != nil {
+				gonic.Gonic(a.AccessError(), ctx)
+				return
+			}
+
+			if namespaceAccess.Access < requiredAccess {
+				gonic.Gonic(a.NotFoundError(), ctx)
+				return
+			}
+
+			rctx := context.WithValue(ctx.Request.Context(), AccessContext, namespaceAccess)
+			ctx.Request = ctx.Request.WithContext(rctx)
 		}
-
-		if namespaceAccess.Access < requiredAccess {
-			gonic.Gonic(a.NotFoundError(), ctx)
-			return
-		}
-
-		rctx := context.WithValue(ctx.Request.Context(), AccessContext, namespaceAccess)
-		ctx.Request = ctx.Request.WithContext(rctx)
 	}
 }
 
